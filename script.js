b7eb840d2fd1d9534a40b454576cc469790b923a
Semon Raja <semonraja4@gmail.com>
2026-06-16 14:52:36 +0530

    Updated portfolio pages and styles

--- script.js
+++ script.js
@@ -1,132 +1,81 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    // Scroll events
-
-    $(window).scroll(function(){
-
-        // Sticky navbar
-
-        if(this.scrollY > 20){
-
+    /* ===== STICKY NAVBAR + SCROLL-UP BUTTON ===== */
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
-
-        }else{
-
+        } else {
             $('.navbar').removeClass("sticky");
-
         }
 
-
-        // Show scroll-up button
-
-        if(this.scrollY > 500){
-
+        if (this.scrollY > 500) {
             $('.scroll-up-btn').addClass("show");
-
-        }else{
-
+        } else {
             $('.scroll-up-btn').removeClass("show");
-
         }
 
+        // Active nav link on scroll (single page only)
+        if ($('.home').length) {
+            var scrollPos = $(window).scrollTop() + 80;
+            $('section').each(function () {
+                var id     = $(this).attr('id');
+                var top    = $(this).offset().top;
+                var bottom = top + $(this).outerHeight();
+                if (scrollPos >= top && scrollPos < bottom) {
+                    $('.navbar .menu li a').removeClass('active');
+                    $('.navbar .menu li a[href="#' + id + '"]').addClass('active');
+                }
+            });
+        }
     });
 
-
-    // Scroll-up button click
-
-    $('.scroll-up-btn').click(function(){
-
-        $('html, body').animate({
-
-            scrollTop:0
-
-        }, 500);
-
+    /* ===== SCROLL-UP BUTTON CLICK ===== */
+    $('.scroll-up-btn').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 500);
     });
 
-
-    // toggle menu/navbar section
-
-    $('.menu-btn').click(function(){
-
+    /* ===== HAMBURGER MENU ===== */
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
-
         $('.menu-btn i').toggleClass("active");
-
     });
 
-    // close mobile menu after clicking a link
-
-    $('.navbar .menu li a').click(function(){
-
+    // Close mobile menu after clicking a link
+    $('.navbar .menu li a').click(function () {
         $('.navbar .menu').removeClass("active");
-
         $('.menu-btn i').removeClass("active");
-
-    });
-    // typing animation script
-
-    if($('.typing').length){
-
-    new Typed(".typing", {
-
-        strings: [
-
-            "Software Engineer",
-
-            "Full Stack Developer",
-
-            "Java Developer",
-
-            "SQL Developer"
-
-        ],
-
-        typeSpeed:100,
-
-        backSpeed:60,
-
-        loop:true
-
     });
 
-}
-    // Owl Carousel
+    /* ===== TYPING ANIMATION (index only) ===== */
+    if ($('.typing').length) {
+        new Typed(".typing", {
+            strings: [
+                "Software Engineer",
+                "Full Stack Developer",
+                "Java Developer",
+                "SQL Developer"
+            ],
+            typeSpeed: 100,
+            backSpeed: 60,
+            loop: true
+        });
+    }
 
-    if($('.carousel').length){
-
-    $('.carousel').owlCarousel({
-
-        margin:20,
-
-        loop:true,
-
-        autoplay:true,
-
-        autoplayTimeout:2000,
-
-        autoplayHoverPause:true,
-
-        dots:true,
-
-        responsive:{
-
-            0:{
-                items:1
-            },
-
-            600:{
-                items:2
-            },
-
-            1000:{
-                items:3
+    /* ===== OWL CAROUSEL (projects) ===== */
+    if ($('.carousel').length) {
+        $('.carousel').owlCarousel({
+            margin: 20,
+            loop: true,
+            autoplay: true,
+            autoplayTimeout: 2500,
+            autoplayHoverPause: true,
+            dots: true,
+            responsive: {
+                0:    { items: 1 },
+                600:  { items: 2 },
+                1000: { items: 3 }
             }
-
-        }
-
-    });
-
-}
+        });
+    }
 
 });
